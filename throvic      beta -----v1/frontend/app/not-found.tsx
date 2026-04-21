bd6a0b88375a712655f9e024ae4f01d9eeb970f8
yash trivedi <105619630+yash635644@users.noreply.github.com>
2026-04-21 18:41:26 +0530

    Delete throvic      beta -----v1 directory

--- throvic      beta -----v1/frontend/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column', color: '#fff', background: '#0d0d14' }}>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-    </div>
-  );
-}
